53865aad243d03a6822ed7a5a0165315aea00283
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-07-23 15:34:51 -0300

    remoção de espaços desnecessários

--- carros_e_vc/src/hooks/useAuthentication.jsx
+++ carros_e_vc/src/hooks/useAuthentication.jsx
@@ -13,9 +13,7 @@ import {
 
 import { useState, useEffect } from "react"
 
-
 export const useAuthentication = () => {
-
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(null)
 
@@ -28,7 +26,6 @@ export const useAuthentication = () => {
         }
     }
 
-    
     //Register
     const createUser = async (data) => {
         checkIfIsCancelled()
@@ -37,95 +34,82 @@ export const useAuthentication = () => {
         setError(null)
 
         try {
-
             const { user } = await createUserWithEmailAndPassword(
                 auth,
                 data.email,
                 data.password
             )
 
-            await updateProfile(user, {displayName: data.displayName})
+            await updateProfile(user, { displayName: data.displayName })
 
             setLoading(false)
 
             return user
-
         } catch (error) {
-
             console.log(error.message)
             console.log(typeof error.message)
 
-            let systemErrorMessage;
+            let systemErrorMessage
 
             if (error.message.includes("Password")) {
-                systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
-                
+                systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres."
+
             } else if (error.message.includes("email-already")) {
-                systemErrorMessage = "E-mail já cadastrado.";
+                systemErrorMessage = "E-mail já cadastrado."
 
             } else {
-                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
             }
 
             setLoading(false)
             setError(systemErrorMessage)
         }
-
     }
-
 
     //Logout - Sign Out
     const logout = () => {
         checkIfIsCancelled()
-
         signOut(auth)
     }
 
     //Login - Sign in
     const login = async (data) => {
         checkIfIsCancelled()
-
         setLoading(true)
         setError(false)
 
         try {
-
             await signInWithEmailAndPassword(auth, data.email, data.password)
-
             setLoading(false)
-
+            
         } catch (error) {
-
-            let systemErrorMessage;
+            let systemErrorMessage
             console.log(error)
 
             if (error.message.includes("invalid-credential")) {
                 systemErrorMessage = "Usuário ou senha incorretos"
-
             } else {
-                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde"
-
+                systemErrorMessage =
+                    "Ocorreu um erro, por favor tente mais tarde"
             }
 
             setError(systemErrorMessage)
+            
+        } finally {
             setLoading(false)
         }
     }
 
-
     useEffect(() => {
-        return () => setCancelled(true);
-    }, []);
+        return () => setCancelled(true)
+    }, [])
 
-
-  return {
-    auth,
-    createUser,
-    error,
-    logout,
-    login,
-    loading
-  }
-
+    return {
+        auth,
+        createUser,
+        error,
+        logout,
+        login,
+        loading,
+    }
 }
-
